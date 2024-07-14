de500ea8dfd52d6fe5717ff2f65f0d392bab6e59
shyam-rajput1487 <shyammohan.off@gmail.com>
2024-07-14 04:31:17 +0000

    basic changes

--- 0_1basic/test.js
+++ 0_1basic/test.js
@@ -1,1 +1,16 @@
-console.log("Shyam");
+// console.log("Shyam")
+// 01_variables
+const accountId=1431
+let accountEmail="shyam@google.com"
+var accountPassword="1143"
+acccountCity ="Lucknow"
+let accountState
+// accountId=3; // not allowed
+accountEmail="shyam@email.com"
+accountPassword="112233"
+acccountCity="Faizabad"
+/*
+Prefer not use var because of issue in block and functional scope
+*/
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,acccountCity,accountState])
